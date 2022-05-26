1bca56ef5503b9439d986112a7e8c49094252857
pkyR <pikytdv@gmail.com>
2022-05-26 15:03:59 -0300

    Agregando enunciado del desafio

--- retos/02-Map/index.js
+++ retos/02-Map/index.js
@@ -1,3 +1,17 @@
+/*
+	En este desafío tienes un array de números, 
+	usando la función map debes retornar 
+	todos los números del array multiplicados por dos.
+
+	La solución debería tener un input y output como los siguientes:
+
+	Input
+	solution([2, 4, 5, 6]);
+
+	Output
+	[4,8,10,12]
+*/
+
 function solution(array) {
 	return array.map(item => item * 2);
 }
