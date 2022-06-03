db3bdcae0d7db65c6df23d14d281a628607d6ace
pikyr <pikytdv@gmail.com>
2022-06-02 23:15:20 -0300

    Fin de clase 18 - includes

---
+++ server/10-includes.js
@@ -0,0 +1,16 @@
+const pets = ['cat', 'dog', 'bat'];
+
+let isInArray = false;
+for (let i = 0; i < pets.length; i++) {
+  const pet = pets[i];
+  if (pet === 'dog') {
+    isInArray = true;
+    break;
+  }
+}
+
+isInArray; // true
+
+const isInArray2 = pets.includes('cat');
+
+console.log(isInArray2);
